0f49663e121fe5a37c1ae7e62772161bac992198
surajpatidar1 <surajpatidar498@gmail.com>
2025-04-07 20:00:10 +0530

    update css

--- Arise/src/Components/Navbar/Navbar.jsx
+++ Arise/src/Components/Navbar/Navbar.jsx
@@ -1,45 +1,57 @@
-
-import { useNavigate } from 'react-router-dom'
-import './Navbar.css'
-import { useContext } from 'react';
-import {StoreContext} from "../../Store/Store"
+import { useNavigate } from 'react-router-dom';
+import './Navbar.css';
+import { useContext, useState } from 'react';
+import { StoreContext } from "../../Store/Store";
 
 const Navbar = () => {
-  const {setVideo,setShowService,setShowAbout,setShowContact} = useContext(StoreContext)
+  const { setVideo, setShowService, setShowAbout, setShowContact } = useContext(StoreContext);
+  const [menuOpen, setMenuOpen] = useState(false);
   const navigate = useNavigate();
 
-  const onService = ()=>{
-    setVideo("false")
-   setShowService(true)
-    navigate("/service")
-  }
-  const onContact = ()=>{
-    setVideo("false")
-    setShowContact(true)
-    navigate("/contact")
-  }
-  const onAbout = ()=>{
-    setVideo("false")
-    setShowAbout(true)
-    navigate("/about")
-  }
-  return (
-    <>
-    <div className="container">
-        <div className="logo">
-            <img src="/logo.jpg" alt="" className='logo' />
-        </div>
-        <div className="options">
-            <ul>
-                <li className="option" onClick={onService}>Services</li>
-                <li className="option" onClick={onContact}>Contact Us</li>
-                <li className="option" onClick={onAbout}>About</li>
-            </ul>
-        </div>
-    </div>
-    </>
-   
-  )
-}
+  const onService = () => {
+    setVideo("false");
+    setShowService(true);
+    setMenuOpen(false);
+    navigate("/service");
+  };
 
-export default Navbar
+  const onContact = () => {
+    setVideo("false");
+    setShowContact(true);
+    setMenuOpen(false);
+    navigate("/contact");
+  };
+
+  const onAbout = () => {
+    setVideo("false");
+    setShowAbout(true);
+    setMenuOpen(false);
+    navigate("/about");
+  };
+
+  const toggleMenu = () => {
+    setMenuOpen(prev => !prev);
+  };
+
+  return (
+    <div className="container">
+      <div className="logo">
+        <img src="/logo.jpg" alt="Logo" className="logo" />
+      </div>
+
+      <div className="menu-icon" onClick={toggleMenu}>
+        ☰
+      </div>
+
+      <div className={`options ${menuOpen ? 'open' : ''}`}>
+        <ul>
+          <li onClick={onService}>Services</li>
+          <li onClick={onContact}>Contact Us</li>
+          <li onClick={onAbout}>About</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
